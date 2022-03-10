4f9546cc91194e90c8f66dcca668759a4e0ad46b
minjunkim.dev <minjunkim.dev@gmail.com>
2022-03-10 15:58:04 +0900

    feat(new): Implement "User" struct to manage user's Infomation
    User 구조체에는 필수정보와 추가정보가 있으며, 추가정보는 nil 값을 허용함.

--- NetflixSignUp/NetflixSignUp/Model/User.swift
+++ NetflixSignUp/NetflixSignUp/Model/User.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 struct User {
-
+    /* 필수정보 */
+    var identifier: String
+    var password: String
+    /* 추가정보 */
+    var nickname: String?
+    var location: String?
+    var referralCode: String?
+    
+    init(identifier: String = "", password: String = "", nickname: String?, location: String?, referralCode: String?) {
+        self.identifier = identifier
+        self.password = password
+        self.nickname = nickname
+        self.location = location
+        self.referralCode = referralCode
+    }
 }
