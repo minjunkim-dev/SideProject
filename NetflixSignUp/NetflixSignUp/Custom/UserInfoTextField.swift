9d8dfbf4c9701fb2b208273b393f6fafd0d4fa7a
minjunkim.dev <minjunkim.dev@gmail.com>
2022-03-10 17:15:55 +0900

    style: Set TextFields for user info input on SignUp Scene
    유저 정보를 입력받기 위한 텍스트필드의 UI가 모두 동일하므로,
    재사용하기 위해 UITextField를 상속받아 커스텀한 이후 사용함.
    UIStackView에 커스텀한 텍스트필드들을 넣고, 이를 씬에 보여줌.

---
+++ NetflixSignUp/NetflixSignUp/Custom/UserInfoTextField.swift
@@ -0,0 +1,31 @@
+//
+//  UserInfoTextField.swift
+//  NetflixSignUp
+//
+//  Created by beneDev on 2022/03/10.
+//
+
+import UIKit
+
+final class UserInfoTextField: UITextField {
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupView()
+    }
+    
+    private func setupView() {
+        
+        let color = UIColor.white
+        self.textColor = color
+        
+        self.textAlignment = .center
+        self.backgroundColor = .darkGray
+        self.borderStyle = .roundedRect
+    }
+}
--- NetflixSignUp/NetflixSignUp/View/SignUpView.swift
+++ NetflixSignUp/NetflixSignUp/View/SignUpView.swift
@@ -13,10 +13,60 @@ import Then
 final class SignUpView: UIView {
  
     let titleLabel = UILabel().then {
+        $0.text = "JUNFLIX"
         $0.textColor = .red
         $0.textAlignment = .center
         $0.font = .systemFont(ofSize: 40, weight: .heavy)
     }
+    
+    
+    /* 필수정보 */
+    let idTextField = UserInfoTextField().then {
+        $0.attributedPlaceholder = NSAttributedString(
+            string: "이메일 주소 또는 전화번호",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.white]
+        )
+        $0.keyboardType = .emailAddress
+    }
+    let pwTextField = UserInfoTextField().then {
+        $0.attributedPlaceholder = NSAttributedString(
+            string: "비밀번호",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.white]
+        )
+        $0.keyboardType = .default
+        $0.isSecureTextEntry = true
+    }
+    
+    /* 추가정보 */
+    let nicknameTextField = UserInfoTextField().then {
+        $0.attributedPlaceholder = NSAttributedString(
+            string: "닉네임",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.white]
+        )
+        $0.keyboardType = .default
+    }
+    let locationTextField = UserInfoTextField().then {
+        $0.attributedPlaceholder = NSAttributedString(
+            string: "위치",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.white]
+        )
+        $0.keyboardType = .default
+    }
+    let referralTexrField = UserInfoTextField().then {
+        $0.attributedPlaceholder = NSAttributedString(
+            string: "추천 코드 입력",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.white]
+        )
+        $0.keyboardType = .default
+    }
+    
+    let stackView = UIStackView().then {
+        $0.alignment = .center
+        $0.distribution = .equalSpacing
+        $0.spacing = 20
+        $0.axis = .vertical
+    }
+    
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -32,7 +82,16 @@ final class SignUpView: UIView {
         backgroundColor = .black
         
         addSubview(titleLabel)
-        titleLabel.text = "JUNFLIX"
+        
+        
+        addSubview(stackView)
+        [
+            idTextField, pwTextField,
+            nicknameTextField, locationTextField, referralTexrField,
+            
+        ]
+            .forEach { stackView.addArrangedSubview($0) }
+        
         
     }
     
@@ -40,8 +99,26 @@ final class SignUpView: UIView {
         
         titleLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.centerY.equalToSuperview().multipliedBy(0.5)
-            $0.horizontalEdges.lessThanOrEqualToSuperview().inset(20)
+            $0.centerY.equalToSuperview().multipliedBy(0.4)
+            $0.horizontalEdges.lessThanOrEqualToSuperview().inset(40)
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview()
+            $0.horizontalEdges.lessThanOrEqualToSuperview().inset(40)
+        }
+        
+        [
+            idTextField, pwTextField,
+            nicknameTextField, locationTextField, referralTexrField,
+            
+        ].forEach {
+            $0.snp.makeConstraints {
+                $0.centerX.equalToSuperview()
+                $0.horizontalEdges.equalToSuperview()
+                $0.height.equalTo(40)
+            }
         }
     }
 }
