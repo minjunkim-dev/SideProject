8fd82e5ccb8a7409852ac159fdbb21ada6c0683d
minjunkim.dev <minjunkim.dev@gmail.com>
2022-03-15 14:01:34 +0900

    chore: Remove storyboards, Prepare resources for use, Set device orientation
    코드로 UI를 잡기 위해 main 스토리보드를 제거함.
    앱 아이콘 및 스플래시에 사용할 리소스를 준비함.
    전광판 앱이라 세로모드는 필요 없을 것이라고 처음에는 생각했지만,
    특수한 경우에 사용자가 필요할지도 모르므로 세로모드도 추가함.

---
+++ LEDBoard/LEDBoard/LEDBoardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LEDBoardViewController.swift
+//  LEDBoard
+//
+//  Created by beneDev on 2022/03/14.
+//
+
+import UIKit
+
+class LEDBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
