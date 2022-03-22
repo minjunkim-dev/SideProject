bcb9b8f8f4d6c5de0c5caa516dcfdd4786650fe1
minjunkim.dev <minjunkim.dev@gmail.com>
2022-03-22 15:12:02 +0900

    fix: Solves the issue that the height of the cell representing the hashtag changes according to the text length after updating the hashtag
    신조어 검색에 성공시, 해시태그가 갱신되는데 이때 각 셀의 텍스트 길이에 따라 높이가 변경되어 보기 불편한 이슈를 발견함.
    
    컬렉션뷰 셀을 self-sizing 셀로 구성하였기 때문에 발생한 현상인데 이 앱의 경우 height는 고정되고 width만 유동적이여야 하므로,
    컬렉션뷰 셀의 contentView의 높이를 특정 값으로 고정시켜 문제를 해결함.

--- newlyCoinedWord/NewlyCoinedWord/Cell/HashTagCollectionViewCell.swift
+++ newlyCoinedWord/NewlyCoinedWord/Cell/HashTagCollectionViewCell.swift
@@ -20,6 +20,7 @@ final class HashTagCollectionViewCell: UICollectionViewCell, ViewPresentable {
         $0.layer.borderWidth = 1
         $0.layer.borderColor = UIColor.black.cgColor
         $0.layer.cornerRadius = 10
+        $0.titleLabel?.numberOfLines = 1
         
         if #available(iOS 15.0, *) {
             var config = UIButton.Configuration.plain()
@@ -31,12 +32,8 @@ final class HashTagCollectionViewCell: UICollectionViewCell, ViewPresentable {
     }
     
     func configureCell(newlyCoinedWord: String) {
-        
-        print(#function)
-        
-        button.titleLabel?.numberOfLines = 1
+    
         button.setTitle(newlyCoinedWord, for: .normal)
-        
     }
     
     @objc func buttonClicked(_ sender: UIButton) {
@@ -51,6 +48,10 @@ final class HashTagCollectionViewCell: UICollectionViewCell, ViewPresentable {
     }
     
     func setupConstraints() {
+        
+        contentView.snp.makeConstraints {
+            $0.height.equalTo(30)
+        }
         
         button.snp.makeConstraints {
             $0.edges.equalToSuperview()
