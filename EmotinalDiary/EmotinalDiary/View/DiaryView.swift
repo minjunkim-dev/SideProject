788a9be4deec8942fc7a32fc47fc603a00927129
minjunkim.dev <minjunkim.dev@gmail.com>
2022-03-25 21:38:07 +0900

    chore: Add resources to use on the project

--- EmotinalDiary/EmotinalDiary/View/DiaryView.swift
+++ EmotinalDiary/EmotinalDiary/View/DiaryView.swift
@@ -10,6 +10,18 @@ import UIKit
 import SnapKit
 import Then
 
+/*
+1. 즐거워 = fun
+2. 기뻐 = happy
+3. 사랑스러워 = lovely
+4. 화나 = angry
+5. 무력해 = helpless
+6. 피곤해 = tired
+7. 난감해 = embarrassing
+8. 우울해 = depressed
+9. 속상해 = upset
+ */
+
 class DiaryView: UIView, ViewPresentable {
     
     func setupView() {
