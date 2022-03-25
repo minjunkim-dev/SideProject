00b8cf7209cac32c0ac79220daeb968fefe9f32b
minjunkim.dev <minjunkim.dev@gmail.com>
2022-03-25 15:05:26 +0900

    fix: Fix an issue where the UserDefaults doesn't not work properly for default data types, not custom data types
    UserDefaults를 비교적 간편하게 사용하기 위해 property wrapper를 활용하였는데,
    여기서 커스텀 자료형에 대해서는 get, set 연산이 정상적으로 동작하나
    기본 자료형(정확히 말하면 NSDate, NSString, NSNumber, NSData, NSArray, NSDictionary 클래스 타입, 스위프트에서 제공하는 기본 자료형)에 대해서는
    get, set 연산이 정확하게 동작하지 않음을 확인하고 이를 수정함.
    
    PropertyList encoder, decoder를 JSON encoder, decoder로 변경하고,
    예외처리를 명확히 하여 기본 자료형에 대해 비정상적으로 동작하는 이슈를 수정함.

--- newlyCoinedWord/NewlyCoinedWord/Extension/UserDefaults+Extension.swift
+++ newlyCoinedWord/NewlyCoinedWord/Extension/UserDefaults+Extension.swift
@@ -14,28 +14,45 @@ struct UserDefault<T: Codable> {
     
     private var container: UserDefaults = .standard
     
+    private let encoder = JSONEncoder()
+    private let decoder = JSONDecoder()
+    
     init(key: String, defaultValue: T) {
         self.key = key
         self.defaultValue = defaultValue
     }
     
     var wrappedValue: T {
+        
         get {
-//            return container.object(forKey: key) as? T ?? defaultValue
-            if let data = container.value(forKey: key) as? Data {
+            
+            if let data = container.object(forKey: key) as? Data {
+        
                 do {
-                    return try PropertyListDecoder().decode(T.self, from: data)
-                } catch {
-                    return defaultValue
+                    return try decoder.decode(T.self, from: data)
+                } catch{
+                    print("get에서 decoding 실패!")
+                    return data as? T ?? defaultValue
                 }
+        
             } else {
-                return defaultValue
+                print("get에서 Data로 가져오기 실패!")
+                return container.object(forKey: key) as? T ?? defaultValue
             }
+      
         }
         set {
-            //            container.set(newValue, forKey: key)
-            container.set(try? PropertyListEncoder().encode(newValue), forKey: key)
             
+            var data: Data
+                
+            do {
+                data = try encoder.encode(newValue)
+                print("set에서 encoding 성공!")
+                container.set(data, forKey: key)
+            } catch {
+                print("set에서 encoding 실패!")
+                container.set(newValue, forKey: key)
+            }
         }
     }
     
@@ -49,9 +66,11 @@ extension UserDefaults {
     @UserDefault(key: "wordList", defaultValue: [])
     static var wordList: [NewlyCoinedWord]
     
+    @UserDefault(key: "isFetchWordListOnFirstDayOfMonth", defaultValue: false)
+    static var isFetchWordListOnFirstDayOfMonth: Bool
+    
     // wrappedValue = projectedValue
     static func resetUserDefaults() {
-        
         wordList = $wordList
     }
 }
