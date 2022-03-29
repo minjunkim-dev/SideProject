ce0034cdbcba6e2f50cba89272bfe8896ba1b035
minjunkim.dev <minjunkim.dev@gmail.com>
2022-03-29 22:58:33 +0900

    chore: Change project settings to set supported platforms, version settings, device orientation

---
+++ PracticeTableViewController/PracticeTableViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeTableViewController
+//
+//  Created by beneDev on 2022/03/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
