02422df213bc9d7f79cc198f6550683cdc4b9716
minjunkim.dev <minjunkim.dev@gmail.com>
2022-03-30 16:15:17 +0900

    feat(new): Add a checkbox button inside a cell to check if a task has been completed
    할 일 리스트에서 할 일을 완료했는지 여부를 알 수 있도록 체크박스 버튼을 셀 내에 추가함.

--- ToDoList/ToDoList/Cell/ToDoTableViewCell.swift
+++ ToDoList/ToDoList/Cell/ToDoTableViewCell.swift
@@ -12,6 +12,13 @@ import Then
 
 final class ToDoTableViewCell: UITableViewCell, ViewPresentable {
 
+    let checkButton = UIButton().then {
+        let image = UIImage(systemName: "checkmark.square")
+        $0.setImage(image, for: .normal)
+        $0.tintColor = .black
+        $0.setPreferredSymbolConfiguration(.init(pointSize: 20), forImageIn: .normal)
+    }
+    
     let contentLabel = UILabel().then {
         $0.textColor = .black
         $0.numberOfLines = 0
@@ -29,23 +36,41 @@ final class ToDoTableViewCell: UITableViewCell, ViewPresentable {
     }
     
     func setupView() {
-        contentView.addSubview(contentLabel)
+        
+        [checkButton, contentLabel].forEach {
+            contentView.addSubview($0)
+        }
     }
     
     func setupConstraints() {
         
+        checkButton.snp.makeConstraints {
+            $0.centerY.equalTo(contentLabel)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
         contentLabel.snp.makeConstraints {
-            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.leading.equalTo(checkButton.snp.trailing).offset(20)
+            $0.trailing.equalToSuperview().inset(20)
             $0.verticalEdges.equalToSuperview().inset(10)
         }
     }
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    @objc private func checkButtonClicked(_ sender: UIButton) {
         
+        sender.isSelected.toggle()
+        
+        let image = sender.isSelected ? UIImage(systemName: "checkmark.square.fill") : UIImage(systemName: "checkmark.square")
+        sender.setImage(image, for: .normal)
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         setupView()
         setupConstraints()
+        
+        checkButton.addTarget(self, action: #selector(checkButtonClicked(_:)), for: .touchUpInside)
     }
     
     required init?(coder: NSCoder) {
