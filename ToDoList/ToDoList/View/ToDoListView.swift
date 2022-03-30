b661acef2ea659085670195b399ddae748b45069
minjunkim.dev <minjunkim.dev@gmail.com>
2022-03-30 14:21:28 +0900

    style: Add a text field for inputting to-do and a button for adding to-do to the list
    컨테이너 역할을 할 UIView를 상위뷰로 하고, 상위뷰에 텍스트필드와 버튼을 하위뷰로 추가하여 UI를 설정함.
    
    텍스트필드 입력중에 기존의 텍스트를 한번에 삭제할 수 있는 clear button이 나타나도록
    clearButtonMode = .whileEditing 코드를 추가함

--- ToDoList/ToDoList/View/ToDoListView.swift
+++ ToDoList/ToDoList/View/ToDoListView.swift
@@ -7,14 +7,59 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class ToDoListView: UIView, ViewPresentable {
+    
+    let containerView = UIView().then {
+        $0.layer.cornerRadius = 10
+    }
+    
+    let toDoTextField = UITextField().then {
+        $0.placeholder = "무엇을 구매하실건가요?"
+        $0.autocapitalizationType = .none
+        $0.clearButtonMode = .whileEditing
+    }
+    
+    let addButton = UIButton().then {
+        $0.setTitle("추가", for: .normal)
+        $0.setTitleColor(.black, for: .normal)
+        $0.layer.cornerRadius = 10
+    }
     
     func setupView() {
         backgroundColor = .white
+        
+        addSubview(containerView)
+        containerView.backgroundColor = .systemGray5
+        
+        [toDoTextField, addButton].forEach {
+            containerView.addSubview($0)
+        }
+        
+        addButton.backgroundColor = .systemGray4
     }
     
     func setupConstraints() {
         
+        containerView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide).inset(20)
+            $0.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(UIScreen.main.bounds.height / 10)
+        }
+        
+        addButton.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview().inset(20)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.width.equalTo(addButton.snp.height).multipliedBy(1.5)
+        }
+        
+        toDoTextField.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(20)
+            $0.verticalEdges.equalTo(addButton)
+            $0.trailing.equalTo(addButton.snp.leading).offset(-20)
+        }
     }
     
     override init(frame: CGRect) {
