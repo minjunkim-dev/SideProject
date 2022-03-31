55bcd0cd8c2b688b650b88ed9fa0aea9a241e714
minjunkim.dev <minjunkim.dev@gmail.com>
2022-03-31 11:20:44 +0900

    feat(improve): Fix cell deletion function, Add cell pinned function
    셀 삭제 기능의 경우 기존에는 메서드를 통해
    1. 셀의 수정이 가능하게 하고(canEditRowAt가 true를 반환)
    2. 수정 스타일이 delete인 경우(commit 메서드에서 editingStyle == .delete인 경우 삭제 수행)
    삭제 기능을 구현함.
    
    그러나 셀 고정 기능과 대칭을 이루기 위해서도 그렇고, delete에 이미지를 넣기 위해서
    trailingSwipeActionsConfigurationForRowAt 메서드에서 UIContextualAction 객체를 생성해
    오른쪽에서 스와이프 했을 때 휴지통 이미지가 나오게끔 하고,
    휴지통을 클릭하면 데이터 및 셀이 사라지도록 구현함.
    
    셀 고정 기능 역시 leadingSwipeActionsConfigurationForRowAt 메서드에서 UIContextualAction 객체를 생성하여
    왼쪽에서 스와이프하여 핀 이미지가 등장하게 하고 클릭시에 고정 여부에 따라 알맞은 섹션에 해당 데이터 및 셀이 위치하게끔 구현함.

--- ToDoList/ToDoList/View/ToDoListViewController.swift
+++ ToDoList/ToDoList/View/ToDoListViewController.swift
@@ -55,9 +55,7 @@ extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource, UI
             return UITableViewCell()
         }
         
-        let section = indexPath.section
-        let row = indexPath.row
-        
+        let (section, row) = (indexPath.section, indexPath.row)
         
         let toDo = section == 0 ? isPinnedMock[row] :  isNotPinnedMock[row]
         cell.configureCell(toDo: toDo)
@@ -78,10 +76,8 @@ extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource, UI
     
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         
-        let sourceSection = sourceIndexPath.section
-        let sourceRow = sourceIndexPath.row
-        let destinationSection = destinationIndexPath.section
-        let destinationRow = destinationIndexPath.row
+        let (sourceSection, sourceRow) = (sourceIndexPath.section, sourceIndexPath.row)
+        let (destinationSection, destinationRow) = (destinationIndexPath.section, destinationIndexPath.row)
        
         var data = sourceSection == 0 ? isPinnedMock.remove(at: sourceRow) : isNotPinnedMock.remove(at: sourceRow)
         data.isPinned.toggle()
@@ -94,8 +90,8 @@ extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource, UI
 
         let dragItem = UIDragItem(itemProvider: NSItemProvider())
 
-        let section = indexPath.section
-        let row = indexPath.row
+        let (section, row) = (indexPath.section, indexPath.row)
+        
         section == 0 ? (dragItem.localObject = isPinnedMock[row]) : (dragItem.localObject = isNotPinnedMock[row])
 
         return [dragItem]
@@ -104,18 +100,44 @@ extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource, UI
 
     
     
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let (section, row) = (indexPath.section, indexPath.row)
+        
+        var data = section == 0 ? isPinnedMock[row] : isNotPinnedMock[row]
+        
+        let pin = UIContextualAction(style: .normal, title: nil) { action, view, completion in
+            
+            data.isPinned ? self.isPinnedMock.remove(at: row) : self.isNotPinnedMock.remove(at: row)
+            data.isPinned.toggle()
+            data.isPinned ? self.isPinnedMock.insert(data, at: 0) : self.isNotPinnedMock.insert(data, at: 0)
+        
+            let range = 0..<tableView.numberOfSections
+            tableView.reloadSections(IndexSet(integersIn: range), with: .automatic)
+            completion(true)
+        }
+        
+        data.isPinned ? (pin.image = UIImage(systemName: "pin.slash.fill")) : (pin.image = UIImage(systemName: "pin.fill"))
+        pin.backgroundColor = .orange
+            
+        return UISwipeActionsConfiguration(actions: [pin])
     }
     
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
-        let section = indexPath.section
-        let row = indexPath.row
-        
-        if editingStyle == .delete {
-            section == 0 ? isPinnedMock.remove(at: row) : isNotPinnedMock.remove(at: row)
+        let (section, row) = (indexPath.section, indexPath.row)
+    
+        let delete = UIContextualAction(style: .destructive, title: nil) { action, view, completion in
+            
+            section == 0 ? self.isPinnedMock.remove(at: row) : self.isNotPinnedMock.remove(at: row)
             tableView.reloadSections(IndexSet(integer: section), with: .automatic)
+            completion(true)
         }
+            
+        delete.image = UIImage(systemName: "trash.fill")
+        delete.backgroundColor = .red
+        
+        return UISwipeActionsConfiguration(actions: [delete])
     }
 }
