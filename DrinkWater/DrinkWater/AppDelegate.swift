7589a8a6bc6a72fbfb0bb98d9b04ad44c9501e43
minjunkim.dev <minjunkim.dev@gmail.com>
2022-04-04 19:51:55 +0900

    fix: Change supported device orientation because only supporting portrait mode seems to be sufficient

--- DrinkWater/DrinkWater/AppDelegate.swift
+++ DrinkWater/DrinkWater/AppDelegate.swift
@@ -32,7 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-        return [.all]
+        return [.portrait]
     }
 }
 
