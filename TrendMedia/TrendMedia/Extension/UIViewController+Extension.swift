19287e69dd28b3e9e4b05d7f86a49f0220545b68
minjunkim.dev <minjunkim.dev@gmail.com>
2022-04-11 13:49:09 +0900

    style: Configure navigation using extension

---
+++ TrendMedia/TrendMedia/Extension/UIViewController+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  UIViewController+Extension.swift
+//  TrendMedia
+//
+//  Created by beneDev on 2022/04/11.
+//
+
+import UIKit
+
+extension UIViewController {
+
+    func configureNavigation(title: String? = nil, titleColor: UIColor? = nil, titleFont: UIFont? = nil, leftItems: [UIBarButtonItem]? = nil, rightItems: [UIBarButtonItem]? = nil, barTintColor: UIColor? = nil) {
+        
+            navigationItem.title = title
+        
+            if let titleColor = titleColor, let titleFont = titleFont {
+                navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: titleColor, .font: titleFont]
+            }
+            
+        navigationItem.leftBarButtonItems = leftItems
+        navigationItem.rightBarButtonItems = rightItems
+        
+        if let barTintColor = barTintColor{
+            navigationController?.navigationBar.tintColor = barTintColor
+        }
+    }
+}
--- TrendMedia/TrendMedia/View/SummaryMediaViewController.swift
+++ TrendMedia/TrendMedia/View/SummaryMediaViewController.swift
@@ -18,6 +18,12 @@ final class SummaryMediaViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
+        let title = "TREND MEDIA"
+        let font = UIFont.systemFont(ofSize: 18, weight: .heavy)
+        let leftItem = UIBarButtonItem(image: UIImage(systemName: "list.dash"), style: .plain, target: self, action: nil)
+        let rightItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: nil)
+        
+        configureNavigation(title: title, titleColor: .black, titleFont: font, leftItems: [leftItem], rightItems: [rightItem], barTintColor: .black)
+        
     }
 }
