8165b74622c39f6940638a64eba7e2aafc07db27
minjunkim.dev <minjunkim.dev@gmail.com>
2022-04-14 15:06:36 +0900

    feat(new): Add the function to search media
    insensitive하게 검색이 가능하게 하기 위해 lowercased 메서드를 활용함.
    
    별도로 예외처리를 안하면 유저가 텍스트 맨 앞에 스페이스바를 입력하면
    대부분의 제목이 스페이스바를 포함하므로 모든 미디어가 검색되는 문제가 발생하여,
    이를 예외처리 하기 위해 텍스트의 첫문자가 스페이스이면 검색되지 않게함.

--- TrendMedia/TrendMedia/View/SearchMediaViewController.swift
+++ TrendMedia/TrendMedia/View/SearchMediaViewController.swift
@@ -8,8 +8,14 @@
 import UIKit
 
 class SearchMediaViewController: UIViewController {
-
+    
     private let mainView = SearchMediaView()
+    
+    var media: [TvShow] = [] {
+        didSet {
+            mainView.tableView.reloadData()
+        }
+    }
     
     override func loadView() {
         view = mainView
@@ -20,7 +26,8 @@ class SearchMediaViewController: UIViewController {
         
         mainView.tableView.delegate = self
         mainView.tableView.dataSource = self
-
+        mainView.searchBar.delegate = self
+        
         mainView.dismissButton.addTarget(self, action: #selector(dismissButtonClicked), for: .touchUpInside)
     }
     
@@ -31,10 +38,9 @@ class SearchMediaViewController: UIViewController {
 
 extension SearchMediaViewController: UITableViewDelegate, UITableViewDataSource {
     
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return tvShow.count
+        return media.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -44,14 +50,36 @@ extension SearchMediaViewController: UITableViewDelegate, UITableViewDataSource 
         }
         
         let row = indexPath.row
-        let title = tvShow[row].title
-        let releaseDate = tvShow[row].releaseDate
-        let region = tvShow[row].region
-        let overview = tvShow[row].overview
-        let imagePath = tvShow[row].backdropImage
+        let title = media[row].title
+        let releaseDate = media[row].releaseDate
+        let region = media[row].region
+        let overview = media[row].overview
+        let imagePath = media[row].backdropImage
         
         cell.configureCell(title: title, releaseDate: releaseDate, region: region ,overview: overview, imagePath: imagePath)
         
         return cell
     }
 }
+
+extension SearchMediaViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        guard let text = searchText.first, text != " " else {
+            media = []
+            return
+        }
+        
+        media = tvShow
+            .filter {
+                let title = $0.title.lowercased()
+                if title.contains(searchText.lowercased()) {
+                    return true
+                } else {
+                    return false
+                }
+            }
+    }
+}
+
