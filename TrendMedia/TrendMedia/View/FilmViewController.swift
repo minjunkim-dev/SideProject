c4bc1b99896c108f6be6624b3fb03d6677113a82
minjunkim.dev <minjunkim.dev@gmail.com>
2022-04-16 16:48:58 +0900

    feat(improve): Make the action sheet appear when click the filter button at the top right

--- TrendMedia/TrendMedia/View/FilmViewController.swift
+++ TrendMedia/TrendMedia/View/FilmViewController.swift
@@ -20,7 +20,36 @@ final class FilmViewController: UIViewController {
         
         let title = "영화"
         let font = UIFont.systemFont(ofSize: 18, weight: .semibold)
-        let filterItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: nil)
+        let filterItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filterButtonClicked))
         configureNavigation(title: title, titleColor: .black, titleFont: font, rightItems: [filterItem], barTintColor: .black)
+    }
+    
+    @objc func filterButtonClicked() {
+        
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        
+        let cancleTitle = "취소"
+        let cancleAction = UIAlertAction(title: cancleTitle, style: .cancel) { _ in
+            print("cancle")
+        }
+        alert.addAction(cancleAction)
+        
+    
+        [
+            "메가박스",
+            "롯데시네마",
+            "CGV",
+            "전체보기",
+        ].forEach {
+            let okAction = UIAlertAction(title: $0, style: .default) { _ in
+                print("ok")
+            }
+            
+            alert.addAction(okAction)
+        }
+        
+        
+        present(alert, animated: true, completion: nil)
     }
 }
